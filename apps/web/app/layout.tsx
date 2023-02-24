deb04fd9a93f7fa6c9c511f6372900dfe325816e
PalanikannanM <palanikannan.m2020@vitstudent.ac.in>
2023-02-24 03:24:14 +0000

    navbar example added

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import "../src/styles/globals.css";
 import "@palanikannan1437/rc4-ui/styles.css";
 import "@palanikannan1437/rc4community-navbar/styles.css";
+import NavigationMenuDemo from "./navbar/nav";
 
 export default function RootLayout({
   children,
@@ -8,14 +9,50 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="bg-zinc-900">
-      <body>
-        <div className="flex min-h-screen flex-col items-center justify-center py-2">
-          <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
-            {children}
-          </main>
-        </div>
-      </body>
-    </html>
+      <html lang="en" className="bg-zinc-900">
+        <body>
+          <header className="body-font text-gray-600">
+            <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
+              <a className="title-font mb-4 flex items-center font-medium text-gray-900 md:mb-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="h-10 w-10 rounded-full bg-indigo-500 p-2 text-white"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                </svg>
+                <span className="ml-3 text-xl">Tailblocks</span>
+              </a>
+              <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto">
+                <NavigationMenuDemo />
+              </nav>
+              <button className="mt-4 inline-flex items-center rounded border-0 bg-gray-100 py-1 px-3 text-base hover:bg-gray-200 focus:outline-none md:mt-0">
+                Button
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="ml-1 h-4 w-4"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </button>
+            </div>
+          </header>
+          <div className="flex min-h-screen flex-col items-center justify-center py-2">
+            <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
+              {children}
+            </main>
+          </div>
+        </body>
+      </html>
   );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Button, Card } from "@palanikannan1437/rc4-ui";
 
 const CARD_CONTENT = [
@@ -20,24 +21,26 @@ const CARD_CONTENT = [
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
-        <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
-          Web
-          <span className="block bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent px-2">
-            Turborepo Example
-          </span>
-        </h1>
-        <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
-          <Button />
-        </div>
+    <>
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
+          <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
+            Web
+            <span className="block bg-gradient-to-r from-brandred to-brandblue bg-clip-text px-2 text-transparent">
+              Turborepo Example
+            </span>
+          </h1>
+          <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
+            <Button />
+          </div>
 
-        <div className="mt-12 grid grid-cols-1 sm:grid-cols-3 gap-4 place-content-evenly">
-          {CARD_CONTENT.map((card) => (
-            <Card key={card.title} {...card} />
-          ))}
-        </div>
-      </main>
-    </div>
+          <div className="mt-12 grid grid-cols-1 place-content-evenly gap-4 sm:grid-cols-3">
+            {CARD_CONTENT.map((card) => (
+              <Card key={card.title} {...card} />
+            ))}
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
