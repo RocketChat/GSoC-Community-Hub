d014d9a2e8f169926449d9c9c432aa998f7f13c5
PalanikannanM <palanikannan.m2020@vitstudent.ac.in>
2023-03-31 07:21:44 +0000

    migrated back to react 18 and removing peer deps

--- packages/navbar/dist/server.js
+++ packages/navbar/dist/server.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var chunkOWCFWPAL_js = require('./chunk-OWCFWPAL.js');
+var chunk262UWVGV_js = require('./chunk-262UWVGV.js');
 var p = require('qs');
 
-function l(t=""){return `${process.env.NEXT_PUBLIC_STRAPI_API_URL||"http://localhost:1337"}${t}`}function c(){return chunkOWCFWPAL_js.d(this,arguments,function*(t={},g={}){let o=chunkOWCFWPAL_js.a({headers:{"Content-Type":"application/json"}},t),e=p.stringify({populate:{variant1:{populate:["tile","subTiles"]},variant2:{populate:["components"]},variant3:{populate:["*"]},brandLogo:{populate:["*"]}}}),s=`${l(`/api/nav-datas${e?`?${e}`:""}`)}`,a=yield fetch(s,o);if(!a.ok)throw console.error(a),new Error("An error occured please try again");let r=yield a.json();return r.length>0?r.data[0].attributes:[]})}
+function l(t=""){return `${process.env.NEXT_PUBLIC_STRAPI_API_URL||"http://localhost:1337"}${t}`}function b(){return chunk262UWVGV_js.f(this,arguments,function*(t={},g={}){let o=chunk262UWVGV_js.a({headers:{"Content-Type":"application/json"}},t),e=p.stringify({populate:{variant1:{populate:["tile","subTiles"]},variant2:{populate:["components"]},variant3:{populate:["*"]},brandLogo:{populate:["*"]}}}),s=`${l(`/api/nav-datas${e?`?${e}`:""}`)}`,a=yield fetch(s,o);if(!a.ok)return console.error(a),[];let r=yield a.json();return r.length>0?r.data[0].attributes:[]})}
 
-exports.fetchNavData = c;
+exports.fetchNavData = b;
 exports.getStrapiURL = l;
--- packages/navbar/dist/server.mjs
+++ packages/navbar/dist/server.mjs
@@ -1,6 +1,6 @@
-import { d, a } from './chunk-MIGENICT.mjs';
+import { f, a } from './chunk-HON2WJ2K.mjs';
 import p from 'qs';
 
-function l(t=""){return `${process.env.NEXT_PUBLIC_STRAPI_API_URL||"http://localhost:1337"}${t}`}function c(){return d(this,arguments,function*(t={},g={}){let o=a({headers:{"Content-Type":"application/json"}},t),e=p.stringify({populate:{variant1:{populate:["tile","subTiles"]},variant2:{populate:["components"]},variant3:{populate:["*"]},brandLogo:{populate:["*"]}}}),s=`${l(`/api/nav-datas${e?`?${e}`:""}`)}`,a$1=yield fetch(s,o);if(!a$1.ok)throw console.error(a$1),new Error("An error occured please try again");let r=yield a$1.json();return r.length>0?r.data[0].attributes:[]})}
+function l(t=""){return `${process.env.NEXT_PUBLIC_STRAPI_API_URL||"http://localhost:1337"}${t}`}function b(){return f(this,arguments,function*(t={},g={}){let o=a({headers:{"Content-Type":"application/json"}},t),e=p.stringify({populate:{variant1:{populate:["tile","subTiles"]},variant2:{populate:["components"]},variant3:{populate:["*"]},brandLogo:{populate:["*"]}}}),s=`${l(`/api/nav-datas${e?`?${e}`:""}`)}`,a$1=yield fetch(s,o);if(!a$1.ok)return console.error(a$1),[];let r=yield a$1.json();return r.length>0?r.data[0].attributes:[]})}
 
-export { c as fetchNavData, l as getStrapiURL };
+export { b as fetchNavData, l as getStrapiURL };
