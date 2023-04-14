e3ac3f60f77a933667a1b42766188e6c1c31737c
PalanikannanM <palanikannan.m2020@vitstudent.ac.in>
2023-04-14 10:41:56 +0000

    added necessary schema

---
+++ cms/src/api/variant2/services/variant2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * variant2 service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::variant2.variant2');
---
+++ cms/src/api/variant3/services/variant3.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * variant3 service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::variant3.variant3');
