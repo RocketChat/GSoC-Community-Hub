a048e209d48d90bd6eb5d8fc6de7913a7d27bd79
SingLi <Sing-Li@users.noreply.github.com>
2023-04-19 06:04:06 -0600

    Update deployment config

--- app/ecosystem.config.js
+++ app/ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: 'GSoC-Community-Hub',
+      name: 'rockonpi community',
       script: 'npm',
       args: 'start -- -p 8090',
-      pid_file: '/tmp/GSoC-Community-Hub.pid',
+      pid_file: '/tmp/rockonpi.pid',
       max_restarts: 10,
       autorestarts: true,
-      error_file: 'GSoC-Community-Hub.error.log',
-      out_file: 'GSoC-Community-Hub.log',
+      error_file: 'rockonpi.log',
+      out_file: 'rockonpi.log',
       exec_mode: 'fork',
       instances: 1,
     },
