db4c67e78d2fab978b4fc83aefad9a497edd4006
PalanikannanM <palanikannan.m2020@vitstudent.ac.in>
2023-07-13 15:56:49 +0000

    overriding custom styles examples added

--- apps/web/app/hackernews/page.tsx
+++ apps/web/app/hackernews/page.tsx
@@ -1,13 +1,21 @@
 import HackerNewsComponent from "@palanikannan1437/rc4community-hackernews/HackerNewsComponent";
-import {getHackerNewsPosts} from "@palanikannan1437/rc4community-hackernews/functions";
-
+import { getHackerNewsPosts } from "@palanikannan1437/rc4community-hackernews/functions";
+import "@palanikannan1437/rc4community-hackernews/styles.css";
+// import HackerNewsComponent from "./HackerNewsComponent";
+import styles from './styles.module.css'
 export default async function Home() {
     const posts = await getHackerNewsPosts("blender");
     const posts1 = await getHackerNewsPosts("rocketchat");
     return (
         <>
-            <HackerNewsComponent posts={posts} />
-            <HackerNewsComponent posts={posts1} />
+            <HackerNewsComponent posts={posts} styleOverrides={{
+                main: styles["my-class"],
+                tabsList: 'text-red-500',
+            }} />
+            <HackerNewsComponent posts={posts1} styleOverrides={{
+                main: styles["my-class"],
+                tabsList: 'text-red-500',
+            }} />
         </>
     );
 }
