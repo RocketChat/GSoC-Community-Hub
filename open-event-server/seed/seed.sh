f7e00562c546cedf3d4cee02f5fc1def5d0b47c9
デヴぁんす <sdevanshu90@yahoo.com>
2023-08-18 12:38:40 +0530

    r3

--- open-event-server/seed/seed.sh
+++ open-event-server/seed/seed.sh
@@ -6,7 +6,7 @@ if [ ! -e $INIT_FLAG ];then
     echo "--Cleaning Up the database--"
     
     ls var/log/seed
-    psql -h localhost -U open_event_user open_event -f var/log/seed/seed.sql
+    psql -h postgres -U open_event_user open_event -f var/log/seed/seed.sql
     echo "--Creating SQL files--"
     psql -U open_event_user open_event -f var/log/seed/tmp/seed_event.sql
     psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_speaker.sql
