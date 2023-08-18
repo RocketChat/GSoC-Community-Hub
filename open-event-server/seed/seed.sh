2855e05187a8e1769755596bb47e679c2a82a0fa
デヴぁんす <sdevanshu90@yahoo.com>
2023-08-18 13:26:13 +0530

    r3

--- open-event-server/seed/seed.sh
+++ open-event-server/seed/seed.sh
@@ -8,17 +8,17 @@ if [ ! -e $INIT_FLAG ];then
     ls var/log/seed
     psql -h localhost -p 5432 -U open_event_user open_event -f var/log/seed/seed.sql
     echo "--Creating SQL files--"
-    psql -U open_event_user open_event -f var/log/seed/tmp/seed_event.sql
+    psql -h localhost -p 5432 -U open_event_user open_event -f var/log/seed/tmp/seed_event.sql
     psql -h opev-postgres -p 5432 -U open_event_user open_event -f var/log/seed/tmp/seed_speaker.sql
     psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_ticket.sql
 
-    # psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_open_event.sql
-    # psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_open_speaker.sql
-    # psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_open_ticket.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_open_event.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_open_speaker.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_open_ticket.sql
 
-    # psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_coffee_meet.sql
-    # psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_coffee_meet_speaker.sql
-    # psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_coffee_meet_ticket.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_coffee_meet.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_coffee_meet_speaker.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_coffee_meet_ticket.sql
 
     echo "--Removing dangling SQL files--"
     rm -f var/log/seed/tmp/*.sql
