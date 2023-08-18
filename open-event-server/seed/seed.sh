320a179db441ba9e8e0777432f05196bff814105
デヴぁんす <sdevanshu90@yahoo.com>
2023-08-18 12:11:59 +0530

    r3

--- open-event-server/seed/seed.sh
+++ open-event-server/seed/seed.sh
@@ -4,23 +4,21 @@ if [ ! -e $INIT_FLAG ];then
     sh var/log/seed/sqlc.sh | exit 1
     
     echo "--Cleaning Up the database--"
-    ls
-    ls var
-    ls var/log
+    
     ls var/log/seed
     psql -h opev-postgres -U open_event_user open_event -f var/log/seed/seed.sql
     echo "--Creating SQL files--"
-    psql -h host.docker.internal -U open_event_user open_event -f var/log/seed/tmp/seed_event.sql
-    psql -U open_event_user open_event -f var/log/seed/tmp/seed_speaker.sql
-    psql -U open_event_user open_event -f var/log/seed/tmp/seed_ticket.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_event.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_speaker.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_ticket.sql
 
-    psql -U open_event_user open_event -f var/log/seed/tmp/seed_open_event.sql
-    psql -U open_event_user open_event -f var/log/seed/tmp/seed_open_speaker.sql
-    psql -U open_event_user open_event -f var/log/seed/tmp/seed_open_ticket.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_open_event.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_open_speaker.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_open_ticket.sql
 
-    psql -U open_event_user open_event -f var/log/seed/tmp/seed_coffee_meet.sql
-    psql -U open_event_user open_event -f var/log/seed/tmp/seed_coffee_meet_speaker.sql
-    psql -U open_event_user open_event -f var/log/seed/tmp/seed_coffee_meet_ticket.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_coffee_meet.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_coffee_meet_speaker.sql
+    psql -h opev-postgres -U open_event_user open_event -f var/log/seed/tmp/seed_coffee_meet_ticket.sql
 
     echo "--Removing dangling SQL files--"
     rm -f var/log/seed/tmp/*.sql
