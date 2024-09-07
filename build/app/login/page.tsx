e2de9669583b42023532dc260b5579b338faab18
Smriti Doneria <smritidoneria@gmail.com>
2024-09-08 00:22:46 +0530

    static site generation

--- build/app/login/page.tsx
+++ build/app/login/page.tsx
@@ -1,120 +1,47 @@
-// "use client";
-// import { useState } from 'react';
-// import { useRouter } from 'next/navigation';
-// import { Alert, Button, Card, Container, Form } from 'react-bootstrap';
-// import { NavigationMenuDemo } from 'navbar-cms';
-// import { navItems } from 'src/projects';
-
-// export default function Login() {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [error, setError] = useState<string | null>(null);
-//   const router = useRouter();
-
-//   const handleLogin = async (event: React.FormEvent) => {
-//     event.preventDefault();
-
-//     try {
-//       const response = await fetch('/api/auth/login', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({ email, password }),
-//       });
-
-//       if (response.ok) {
-//         console.log('Login successful');
-//         alert('Login successful');
-//         // Handle success, e.g., redirect to home page
-//         router.push('/home'); // Adjust the redirect path as needed
-//       } else {
-//         // Handle error
-//         const result = await response.json();
-//         setError(result.message || 'An error occurred');
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       setError('An unexpected error occurred');
-//     }
-//   };
-
-//   return (
-//     <>
-//       <NavigationMenuDemo data={navItems} />
-//       <Container className="d-flex justify-content-center align-items-center min-vh-100">
-//         <div className="w-100" style={{ maxWidth: '400px' }}>
-//           <Card>
-//             <Card.Header className="bg-primary text-white text-center">
-//               <h3>Login</h3>
-//             </Card.Header>
-//             <Card.Body>
-//               {error && <Alert variant="danger">{error}</Alert>}
-//               <Form onSubmit={handleLogin}>
-//                 <Form.Group controlId="formEmail">
-//                   <Form.Label>Email address</Form.Label>
-//                   <Form.Control
-//                     type="email"
-//                     placeholder="Enter email"
-//                     value={email}
-//                     onChange={(e) => setEmail(e.target.value)}
-//                     required
-//                   />
-//                 </Form.Group>
-//                 <Form.Group controlId="formPassword" className="mt-3">
-//                   <Form.Label>Password</Form.Label>
-//                   <Form.Control
-//                     type="password"
-//                     placeholder="Password"
-//                     value={password}
-//                     onChange={(e) => setPassword(e.target.value)}
-//                     required
-//                   />
-//                 </Form.Group>
-//                 <Button variant="primary" type="submit" className="mt-4 w-100">
-//                   Login
-//                 </Button>
-//               </Form>
-//               <div className="text-center mt-3">
-//                 <p>Don't have an account? <a href="/register">Register here</a></p>
-//               </div>
-//             </Card.Body>
-//           </Card>
-//         </div>
-//       </Container>
-//     </>
-//   );
-// }
-
 'use client';
 import React, { useState } from 'react';
+import { faunaClient } from 'build/fauna/fauna-client';
 import { useRouter } from 'next/navigation';
 import { Container, Card } from 'react-bootstrap'; // Updated imports
 import { NavigationMenuDemo } from 'navbar-cms';
 import { navItems } from 'src/projects';
-import LoginForm from '../../components/forms/login-form'; // Import the new component
+import LoginForm from '../../components/forms/login-form';
+import { query as q } from 'faunadb'; // Import the new component
 
 export default function Login() {
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
 
+  type UserType = {
+    ref: {
+      id: string;
+    };
+    data: {
+      email: string;
+      password: string;
+    };
+  };
+
   const handleSubmit = async (email: string, password: string) => {
     try {
-      const response = await fetch('/api/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
+      const users: { data: UserType[] } = await faunaClient.query(
+        q.Map(
+          q.Paginate(q.Documents(q.Collection('User'))),
+          q.Lambda('userRef', q.Get(q.Var('userRef')))
+        )
+      );
 
-      if (response.ok) {
-        console.log('Login successful');
+      const user = users.data.find((user: any) => user.data.email === email);
 
-        router.push('/');
+      if (user) {
+        if (user.data.password === password) {
+          console.log('Login successful');
+          router.push('/');
+        } else {
+          setError('Invalid password');
+        }
       } else {
-        const result = await response.json();
-        setError(result.message || 'An error occurred');
+        setError('User not found');
       }
     } catch (error) {
       console.error(error);
