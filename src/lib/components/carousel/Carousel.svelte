e3c122046d9f704306fc5785f732312d008f84d1
r3yc0n1c <raja.majumdar.01@gmail.com>
2025-03-19 22:37:20 +0000

    feat: add new carousel component

--- src/lib/components/carousel/Carousel.svelte
+++ src/lib/components/carousel/Carousel.svelte
@@ -1,56 +1,65 @@
 <script module>
 </script>
 
-<script lang="ts"> 
+<script lang="ts">
 	let { stories } = $props();
+	let n = stories.length;
 
-	import {
-		Container,
-		Row,
-		Col,
-	} from '@sveltestrap/sveltestrap';
+	import { Container, Row, Col } from '@sveltestrap/sveltestrap';
 	import Arrow from './Arrow.svelte';
 
 	import { TinySlider } from 'svelte-tiny-slider';
 	import { Styles } from '@sveltestrap/sveltestrap';
 
- let sw = $state(0);
- let slider = $state("");
- let currentI = $state(1)
+	let sw = $state(0);
 </script>
-{#snippet slides(swidth)}
-  {#each stories as item}
-    <img src={item} width={swidth / 3} alt="no_alt" />
-  {/each}
-{/snippet}
 
-
-{#snippet controls(setI, currentI)}
-          <button class="prev" onclick={() => setI((currentI > 1) ? (currentI - 1) : 0) }>
-            <Arrow direction="left" />
-            </button>
-          <button class="next" onclick={() => setI( (currentI < (items.length - 4)) ? currentI : (currentI + 1) )} >
-            <Arrow direction="right" /> </button>
-{/snippet}
-
-{#snippet tst(sw, ci)}
-          {#each stories as item, i}
-            {#if (i  >=  ci )}
-            <img src={item} width={sw / 3} alt="no_alt" />
-            {/if}
-          {/each}
-{/snippet}
-
-<Styles /> 
+<Styles />
 <div>
 	<Container>
 		<Row>
 			<Col>
-			<TinySlider gap="10px" bind:this={slider} bind:sliderWidth={sw} bind:currentIndex={currentI}>
-       
-      {@render tst(sw, currentI)}
-      {@render controls(slider.setIndex, currentI)}
-      </TinySlider>
+				<div class="tiny-slider-wrapper">
+					<TinySlider gap="10px" bind:sliderWidth={sw}>
+						<!-- items -->
+						{#snippet children({ currentIndex })}
+							{#each stories as item, i}
+								{#if i >= currentIndex}
+									<img src={item} width={sw / 3} alt="no_alt" />
+								{/if}
+							{/each}
+						{/snippet}
+
+						<!-- controls -->
+						{#snippet controls({ setIndex, currentIndex })}
+							<button
+								class="prev"
+								onclick={() => setIndex(currentIndex > 0 ? currentIndex - 1 : 0)}
+							>
+								<Arrow direction="left" />
+							</button>
+
+							<button
+								class="next"
+								onclick={() =>
+									setIndex(currentIndex < n - 4 ? currentIndex : currentIndex + 1)}
+							>
+								<Arrow direction="right" />
+							</button>
+
+							<div class="btn-container">
+								{#each Array(Math.ceil(stories.length / 3)) as _, i}
+									<button
+										class="car-btn"
+										class:active={i == currentIndex}
+										onclick={() => setIndex(i)}
+										aria-label={`Go to page ${i + 1}`}
+									></button>
+								{/each}
+							</div>
+						{/snippet}
+					</TinySlider>
+				</div>
 			</Col>
 		</Row>
 	</Container>
@@ -58,22 +67,51 @@
 
 <style>
 	.prev {
-	  position: absolute; /* Positioned relative to the parent */
+		position: absolute; /* Positioned relative to the parent */
 		left: 0; /* Aligns it to the left edge */
 		top: 50%; /* Moves it halfway down the parent */
 		transform: translateY(-50%); /* Centers it vertically */
 		width: 50px;
 		height: 50px;
-		background-color: white;
+		/* background-color: white; */
 	}
 
 	.next {
-	 position: absolute; /* Positioned relative to the parent */
+		position: absolute; /* Positioned relative to the parent */
 		right: 0; /* Aligns it to the right edge */
 		top: 50%; /* Moves it halfway down the parent */
 		transform: translateY(-50%); /* Centers it vertically */
 		width: 50px;
 		height: 50px;
-		background-color: white;
+		/* background-color: white; */
+	}
+	.tiny-slider-wrapper {
+		position: relative;
+		/* background-color: whitesmoke; */
+		padding-left: 4rem;
+		padding-right: 4rem;
+		padding-top: 2rem;
+		padding-bottom: 1rem;
+	}
+	.btn-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-top: 10px;
+		gap: 8px; /* Add spacing between buttons */
+	}
+
+	.car-btn {
+		background-color: gainsboro;
+		width: 0.5rem;
+		height: 0.5rem;
+		margin: 4px;
+		border-radius: 50%;
+	}
+	.car-btn:hover {
+		background-color: gray;
+	}
+	.active {
+		background-color: gray;
 	}
 </style>
