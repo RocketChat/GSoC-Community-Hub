92895211c7f04ba4098bce52a18cd311445e0283
adaniel105 <dakinola18@gmail.com>
2025-03-23 21:02:38 +0100

    Add main.agml

--- src/compiler/scripts/transform/transformDataItems.ts
+++ src/compiler/scripts/transform/transformDataItems.ts
@@ -1,6 +1,6 @@
 
 export function transformDataItems(dataItems: string[], fileName: string): string {
-    const sourcePath = `../../build`;
+    const sourcePath = fileName == "main" ? `../build` : "../..build";
     return dataItems.map(item => ` import { ${item} } from "${sourcePath}/${item}";`).join("\n");
     //return `import { ${dataItems.join(', ')} } from '${sourcePath}';`;
 }
--- src/routes/community/+page.svelte
+++ src/routes/community/+page.svelte
@@ -1,14 +1,14 @@
 
 <script lang="ts">
-   import { brand } from "../../build/brand";
- import { menutree } from "../../build/menutree";
- import { herounit } from "../../build/herounit";
- import { docslink } from "../../build/docslink";
- import { searchactions } from "../../build/searchactions";
- import { counters } from "../../build/counters";
- import { tilelinks } from "../../build/tilelinks";
- import { stories } from "../../build/stories";
- import { personalities } from "../../build/personalities";
+   import { brand } from "../..build/brand";
+ import { menutree } from "../..build/menutree";
+ import { herounit } from "../..build/herounit";
+ import { docslink } from "../..build/docslink";
+ import { searchactions } from "../..build/searchactions";
+ import { counters } from "../..build/counters";
+ import { tilelinks } from "../..build/tilelinks";
+ import { stories } from "../..build/stories";
+ import { personalities } from "../..build/personalities";
   import Menubar from '$lib/components/menubar/Menubar.svelte';
 import Herounit from '$lib/components/herounit/Herounit.svelte';
 import Docslink from '$lib/components/docslink/Docslink.svelte';
@@ -17,15 +17,7 @@ import Statscounters from '$lib/components/statscounters/Statscounters.svelte';
 import Simonlinktiles from '$lib/components/simonlinktiles/Simonlinktiles.svelte';
 import Carousel from '$lib/components/carousel/Carousel.svelte';
 import Personatiles from '$lib/components/personatiles/Personatiles.svelte';
-let herotitle = herounit[0].herotitle;
-let herosub = herounit[0].herosub;
-let caption = personalities[0].caption;
-let personas = personalities[0].personas;
-let images = personalities[0].images;
-let leftlink = docslink[0].leftlink;
-let rightlink = docslink[0].rightlink;
 </script>
-
 
 <Menubar {brand} {menutree} />
 <Herounit title={herotitle} subtitle={herosub}/>
@@ -35,4 +27,3 @@ let rightlink = docslink[0].rightlink;
 <Simonlinktiles {tilelinks}/>
 <Carousel {stories}/>
 <Personatiles {caption} {personas} {images}/>
-
