7ff9f1072c6d65c7266eda3d75414c429679172b
saianumala <anumalansk@gmail.com>
2025-03-26 13:28:57 +0530

    deleted js files

--- src/compiler/scripts/transform/generateSvelteComponent.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateSvelteComponent = generateSvelteComponent;
-function generateSvelteComponent(importStatement, dataImports, componentName, jsxElements) {
-    var imports = importStatement.join('\n');
-    var data = dataImports;
-    return "\n<script lang=\"ts\">\n  ".concat(data, "\n  ").concat(imports, "\n</script>\n\n").concat(jsxElements, "\n");
-}
--- src/compiler/scripts/transform/transformDataItems.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.transformDataItems = transformDataItems;
-function transformDataItems(dataItems, fileName) {
-    var sourcePath = fileName == "main" ? "../build" : "../../build";
-    return dataItems.map(function (item) { return " import { ".concat(item, " } from \"").concat(sourcePath, "/").concat(item, "\";"); }).join("\n");
-    //return `import { ${dataItems.join(', ')} } from '${sourcePath}';`;
-}
