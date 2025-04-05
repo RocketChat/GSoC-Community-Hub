39cdab5dc09b72596fb1fa5fa212e9e55c01be93
Anjy Gupta <anjyofficial@gmail.com>
2025-04-05 23:10:49 +0530

    fixes

--- src/routes/alumni-summit/+page.svelte
+++ src/routes/alumni-summit/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
    import { brand } from "../../build/brand";
  import { menutree } from "../../build/menutree";
+ import { eventData } from "../../build/eventData";
   import Menubar from '$lib/components/menubar/Menubar.svelte';
 import EventPoster from '$lib/components/eventposter/EventPoster.svelte';
 </script>
