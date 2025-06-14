7e18befce44fe13cb51733d24abd1c9d8a4986bb
adaniel105 <dakinola18@gmail.com>
2025-06-14 10:12:07 +0100

    Add leaderboard page

---
+++ src/routes/leaderboard/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+   import { brand } from "../../build/brand";
+ import { menutree } from "../../build/menutree";
+ import { contributorData } from "$build/contributorData";
+ import { loginData } from "$build/loginData";
+  import Menubar from '$lib/components/menubar/Menubar.svelte';
+import Leaderboard from "$lib/components/leaderboard/Leaderboard.svelte";
+</script>
+
+<Menubar {brand} {menutree} {loginData} />
+<Leaderboard {contributorData} />
