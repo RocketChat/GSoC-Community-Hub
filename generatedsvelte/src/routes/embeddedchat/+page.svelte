d5b8536b2260b4d7aae0cac2a23bbb6c0d00eafb
adaniel105 <dakinola18@gmail.com>
2025-07-30 09:43:28 +0100

    Modify embeddedchat component

--- generatedsvelte/src/routes/embeddedchat/+page.svelte
+++ generatedsvelte/src/routes/embeddedchat/+page.svelte
@@ -2,8 +2,9 @@
    import { brand } from "../../../../src/brand";
     import { menutree } from "../../../../src/menutree";
     import Menubar from '$lib/components/menubar/Menubar.svelte';
+    import { threadsList } from "../../../../build/threadsList"
     import EmbeddedChat from "$lib/components/embeddedchat/EmbeddedChat.svelte";
 </script>
 
 <Menubar {brand} {menutree} />
-<EmbeddedChat />
+<EmbeddedChat threadList={threadsList}/>
