5dc77155b49b0c187f8b6e6984681e7f31300e06
adaniel105 <dakinola18@gmail.com>
2025-07-30 11:02:12 +0100

    Add embeddedchat agml file

--- generatedsvelte/src/lib/util/main.ts
+++ generatedsvelte/src/lib/util/main.ts
@@ -2,7 +2,7 @@ import { fetchStats } from './countStats';
 import { fetchContributors, fetchLastUpdated } from './fetchContributors';
 import {fetchRcStats} from "../util/countRcStats"
 
-fetchRcStats();
-fetchStats();
-fetchContributors();
-fetchLastUpdated();
+// fetchRcStats();
+// fetchStats();
+// fetchContributors();
+// fetchLastUpdated();
--- generatedsvelte/src/routes/embeddedchat/+page.svelte
+++ generatedsvelte/src/routes/embeddedchat/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
    import { brand } from "../../../../src/brand";
-    import { menutree } from "../../../../src/menutree";
-    import Menubar from '$lib/components/menubar/Menubar.svelte';
-    import { threadsList } from "../../../../build/threadsList"
-    import EmbeddedChat from "$lib/components/embeddedchat/EmbeddedChat.svelte";
+ import { menutree } from "../../../../src/menutree";
+ import { threadsList } from "../../../../build/threadsList";
+  import Menubar from '$lib/components/menubar/Menubar.svelte';
+import EmbeddedChat from '$lib/components/embeddedchat/EmbeddedChat.svelte';
 </script>
 
 <Menubar {brand} {menutree} />
