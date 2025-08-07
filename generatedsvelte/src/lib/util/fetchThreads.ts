4253818ddd72b4ca08b17abc9f4e2b19f6d204a4
adaniel105 <dakinola18@gmail.com>
2025-08-08 00:14:01 +0100

    Merge branch 'main' into feat/embeddedchat

--- generatedsvelte/src/lib/util/fetchThreads.ts
+++ generatedsvelte/src/lib/util/fetchThreads.ts
@@ -2,14 +2,13 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
-const ROOM_ID = import.meta.env.VITE_ROOM_ID;
+const ROOM_ID = import.meta.env.VITE_ROOM_ID.split(',');
 const THREAD_COUNT = 5;
-const THREADS_URL = `${BASE_URL}/api/v1/chat.getThreadsList?count=${THREAD_COUNT}&sort=%7B%22tcount%22%3A%20-1%2C%20%22tlm%22%3A%20-1%7D&rid=${ROOM_ID}`;
 const USER_TOKEN = import.meta.env.VITE_USER_TOKEN;
 const USERID = import.meta.env.VITE_USERID;
 const rootDir = path.resolve(process.cwd(), '../');
-const outputFileDir = path.join(rootDir, '/build/threadsList.js');
 const maxRetries = 3;
+
 let validResponse = false;
 
 export interface Threads {
@@ -22,9 +21,10 @@ export interface Threads {
     time_last_replied: string;
 }
 
-let threads : Array<Threads> = [];
-export const fetchThreads = async () => {
-    try{
+const fetchThreads = async (room_id: string) => {
+    let threads : Array<Threads> = [];
+    const THREADS_URL = `${BASE_URL}/api/v1/chat.getThreadsList?count=${THREAD_COUNT}&sort=%7B%22tcount%22%3A%20-1%2C%20%22tlm%22%3A%20-1%7D&rid=${room_id.trim()}`;
+    try{    
         const res = await fetch(THREADS_URL, {
             method: 'GET',
             headers: {
@@ -33,36 +33,39 @@ export const fetchThreads = async () => {
                 accept: 'application/json'
             }
         });
-    
+
         for (let retryCount = 1; retryCount <= maxRetries; retryCount++) {
             if (res.ok) {
                 validResponse = true;
                 break;
             }
         }
+
         if (validResponse) {
-            const data = await res.json(); 
+            const data = await res.json();
             for(const thread in data["threads"]){
-                const threadObj = {
-                    id: data["threads"][thread]["_id"],
-                    message: data["threads"][thread]["msg"],
-                    tcount: data["threads"][thread]["tcount"],
-                    timestamp: data["threads"][thread]["ts"],
-                    username: data["threads"][thread]["u"]["username"],
-                    name: data["threads"][thread]["u"]["name"],
-                    time_last_replied: data["threads"][thread]["tlm"]
-                }
+            const threadObj = {
+                id: data["threads"][thread]["_id"],
+                message: data["threads"][thread]["msg"],
+                tcount: data["threads"][thread]["tcount"],
+                timestamp: data["threads"][thread]["ts"],
+                username: data["threads"][thread]["u"]["username"],
+                name: data["threads"][thread]["u"]["name"],
+                time_last_replied: data["threads"][thread]["tlm"]
+            }
                 threads.push(threadObj);
             }
-            fs.writeFileSync(outputFileDir, `export const threadsList = ${JSON.stringify(threads)}`)
-        } else {
-            fs.writeFileSync(
-                outputFileDir,
-                `export const threadsList = [{"message":"N/A","timestamp":"2025-01-01T01:00:00.415Z", "tcount" : "0",
-                "username":"N/A","name":N/A","time_last_replied":"2025-01-01T01:00:00.415Z"}] `
-            );
+            return threads;
         }
     } catch (error){
         console.error(error);
     }
 };
+
+export const fetchAllThreads = async () => {
+    for(const id in ROOM_ID){
+        let outputFileDir = path.join(rootDir, `/build/_${ROOM_ID[id].trim()}.js`);
+        let threads = await fetchThreads(ROOM_ID[id]);
+        fs.writeFileSync(outputFileDir, `export const _${ROOM_ID[id].trim()} = ${JSON.stringify(threads)}`);
+    }
+}
--- generatedsvelte/src/lib/util/main.ts
+++ generatedsvelte/src/lib/util/main.ts
@@ -1,9 +1,12 @@
 import { fetchStats } from './countStats';
 import { fetchContributors, fetchLastUpdated } from './fetchContributors';
-import {fetchRcStats} from "./countRcStats"
 import { fetchEventData } from './fetchEventData';
+import {fetchRcStats} from "../util/countRcStats"
+import { fetchAllThreads } from './fetchThreads';
 
 fetchRcStats();
 fetchStats();
 fetchContributors();
+fetchEventData();
 fetchLastUpdated();
+fetchAllThreads();
