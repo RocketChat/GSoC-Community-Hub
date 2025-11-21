14bd932e09f071ee1732fea519983f8454a2caee
meet-159 <meetsohani01@gmail.com>
2025-11-21 13:49:49 +0530

    cleaning files

--- generatedsvelte/src/lib/util/main.ts
+++ generatedsvelte/src/lib/util/main.ts
@@ -4,21 +4,15 @@ import { fetchEventData } from './fetchEventData';
 import {fetchRcStats} from "../util/countRcStats"
 import { fetchAllThreads } from './fetchThreads';
 
-
-import { fetchDataFunction as fetchData__meet_159_discor_stats_counter } from "@meet-159/discor-stats-counter";
 async function main(){
-    // fetchRcStats();
-    // fetchStats();
-    // fetchContributors();
-    // fetchEventData();
-    // fetchLastUpdated();
-    // fetchAllThreads();
+    fetchRcStats();
+    fetchStats();
+    fetchContributors();
+    fetchEventData();
+    fetchLastUpdated();
+    fetchAllThreads();
 
 
-
-
-
-    await fetchData__meet_159_discor_stats_counter();
 }
 
 main();
